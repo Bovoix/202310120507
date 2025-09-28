b10382b62c0e9c80a71c1b165f5e4c91a49cdb2a
Bovoix <2048500540@qq.com>
2025-09-28 18:07:32 +0800

    这是第一次的打印素数的文件

---
+++ 初步素数.py
@@ -0,0 +1,19 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+count = 0
+for num in range(1, 20001):
+    if is_prime(num):
+        print(num, end=" ")
+        count += 1
+        if count % 5 == 0:
+            print()
